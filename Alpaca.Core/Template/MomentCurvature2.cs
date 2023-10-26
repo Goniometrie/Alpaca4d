2442d9e56104b62436b6e2e321d0f68d2f223864
TomDiks <tomdiks96@outlook.com>
2023-10-26 21:39:46 +0200

    MK template 2

---
+++ Alpaca.Core/Template/MomentCurvature2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alpaca4d.Core.Template
+{
+    public class MomentCurvature2
+    {
+        public static string Define()
+        {
+            // initialize MomentCurvature tcl file
+            StringBuilder MKtcl = new StringBuilder();
+
+            // Set up
+            MKtcl.Comment(" SET UP ----------------------------------------");
+            MKtcl.Code("wipe");
+
+            return MKtcl.ToString();
+        }
+
+
+    }
+
+    public static class StringBuilderExtensions
+    {
+        public static StringBuilder Comment(this StringBuilder stringBuilder, string comment)
+        {
+            stringBuilder.Append("# " + comment + " \n");
+            return stringBuilder;
+        }
+
+        public static StringBuilder Code(this StringBuilder stringBuilder, string code)
+        {
+            stringBuilder.Append(code + ";\n");
+            return stringBuilder;
+        }
+    }
+
---
+++ Alpaca4d.Gh/11_MomentCurvature/MomentCurvature2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Alpaca4d.Gh._11_MomentCurvature
+{
+    public class MomentCurvature2 : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MomentCurvature2 class.
+        /// </summary>
+        public MomentCurvature2()
+          : base("MomentCurvature2", "MomentCurvature2",
+              "Description",
+              "Alpaca4d", "MomentCurvature")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddBooleanParameter("run", "run", "run script", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            bool run = false;
+            //string dir = "y";
+            DA.GetData(0, ref run);
+
+            if (run) 
+            {
+                string text = Alpaca4d.Template.
+            }
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("0C69C090-E50D-49C6-AC39-4D7D5E121544"); }
+        }
+    }
+}
